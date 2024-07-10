42a3b85df12b83385f4397014272b401a016775f
Deepak Panwar <deepaksinghpanwar111@gmail.com>
2024-07-10 16:31:57 +0530

    fourth chapter

--- server.cpp
+++ server.cpp
@@ -1,63 +1,148 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-#include<string.h>
-#include<errno.h>
-#include<unistd.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netinet/ip.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/ip.h>
 
-static void msg(const char* msg){
-fprintf(stderr,"%s\n",msg);
+const size_t k_max_msg = 4096;
+
+static void msg(const char *msg)
+{
+    fprintf(stderr, "%s\n", msg);
 }
 
-static void die(const char* msg){
-    int err=errno;
-    fprintf(stderr,"[%d] %s",err,msg);
+static void die(const char *msg)
+{
+    int err = errno;
+    fprintf(stderr, "[%d] %s", err, msg);
     abort();
 }
 
-static void do_something(int connfd){
+static void do_something(int connfd)
+{
     char rbuff[64];
-    ssize_t n=read(connfd,rbuff,sizeof(rbuff)-1);
-    
-    if(n<0){
+    ssize_t n = read(connfd, rbuff, sizeof(rbuff) - 1);
+
+    if (n < 0)
+    {
         msg("read() error");
         return;
     }
 
-    printf("client says:%s\n",rbuff);
+    printf("client says:%s\n", rbuff);
 
-    char wbuff[]="World";
-    write(connfd,wbuff,sizeof(wbuff));
+    char wbuff[] = "World";
+    write(connfd, wbuff, sizeof(wbuff));
 }
 
-int main(){
-    int fd=socket(AF_INET,SOCK_STREAM,0);
-    if(fd<0) die("socket()");
+static int32_t read_full(int fd, char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0)
+            return -1;
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
-    int val=1;
-    setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&val,sizeof(val));
+static int32_t write_all(int fd, const char *buf, size_t n)
+{
+    while (n > 0)
+    {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0)
+            return -1;
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
 
-    struct sockaddr_in addr={};
-    addr.sin_family=AF_INET;
-    addr.sin_port=ntohs(1234);
-    addr.sin_addr.s_addr=ntohl(0);
+static int32_t one_request(int connfd)
+{
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(connfd, rbuf, 4);
+    if (err)
+    {
+        if (errno == 0)
+            msg("EOF");
+        else
+            msg("read() error");
+        return err;
+    }
+    uint32_t len = 0;
+    memcpy(&len, rbuf, 4);
 
-    int rv=bind(fd,(const sockaddr *)&addr,sizeof(addr));
-    if(rv<0) die("bind()");
+    if (len > k_max_msg)
+    {
+        msg("too long");
+        return -1;
+    }
+    err = read_full(connfd, &rbuf[4], len);
+    if (err)
+    {
+        msg("read() error");
+        return err;
+    }
+    rbuf[4 + len] = '\0';
+    printf("client says:%s\n", &rbuf[4]);
+    const char reply[] = "World";
+    char wbuf[4 + sizeof(reply)];
+    len = (uint32_t)strlen(reply);
+    memcpy(wbuf, &len, 4);
+    memcpy(&wbuf[4], &reply, len);
+    return write_all(connfd, wbuf, 4 + len);
+}
 
-    rv = listen(fd,SOMAXCONN);
-    if(rv<0) die("listen()");
+int main()
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0)
+        die("socket()");
 
-    while(true){
-        struct sockaddr_in client={};
-        socklen_t socklen=sizeof(client);
+    int val = 1;
+    setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 
-        int connfd=accept(fd,(struct sockaddr*)&client,&socklen);
-        if(connfd<0)continue;
-        do_something(connfd);
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(0);
+
+    int rv = bind(fd, (const sockaddr *)&addr, sizeof(addr));
+    if (rv < 0)
+        die("bind()");
+
+    rv = listen(fd, SOMAXCONN);
+    if (rv < 0)
+        die("listen()");
+
+    while (true)
+    {
+        struct sockaddr_in client = {};
+        socklen_t socklen = sizeof(client);
+
+        int connfd = accept(fd, (struct sockaddr *)&client, &socklen);
+        if (connfd < 0)
+            continue;
+
+        while (true)
+        {
+            int32_t err = one_request(connfd);
+            if (err)
+                break;
+        }
+        // do_something(connfd);
         close(connfd);
     }
     return 0;
